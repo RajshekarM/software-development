cb02eefd4591a181b18c4f4f6725bf722c50db51
Rajashekar Mudigonda <rajashekarmudigonda@dhcp172.merusouth112.iit.edu>
2024-04-05 16:50:28 -0500

    addding the user details

--- node-projects/express-server.js
+++ node-projects/express-server.js
@@ -5,8 +5,34 @@ const express = require("express")
 
 const app = express()
 
-app.get('/', (req, res)=>{
-    res.send('hi there')
+const users = []
+
+const user1 = {
+name : 'john',
+age : '25',
+conidition : 'kidney malfunction',
+cured : false
+};
+
+const user2 = {
+name : 'john',
+age : '30',
+conidition : 'heart malfunction',
+cured : true
+}   
+
+const user3 = {
+name : 'rajesh',
+age : '28',
+condition : 'lung cancer',
+cured : false
+}
+
+users.push(john, david, user3)
+
+
+app.get('/users', (req, res)=>{
+    res.send(users)
 })
 
 app.listen(3000, ()=>{console.log("server started to listen")})
